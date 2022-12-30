481371620d2207eb6d50febffbb22ecddc0c7892
Joshua Blewitt <joshblewitt@protonmail.com>
2022-12-30 22:52:15 +0000

    Day 1 completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code-2022
+
+My work on solving the challenges of the advent of code, 2022 edition.
---
+++ day1.py
@@ -0,0 +1,36 @@
+import csv
+
+elves = []
+calories = []
+
+with open('day1Input.txt') as csvFile:
+	csv_reader = csv.reader(csvFile)
+	line_count = 0
+	addRow = True
+	elf = []
+	for row in csv_reader:
+		if row == []:
+			elves.append(elf)
+			elf = []
+		else:
+			elf.append(f'{", ".join(row)}')
+
+count = 0
+for i in elves:
+	count+=1
+
+print(f'Complete! There are {count} elves')
+
+for elf in elves:
+	caloriesTotal = 0
+	for i in elf:
+		caloriesTotal = caloriesTotal + int(i)
+	calories.append(caloriesTotal)
+print('Before')
+print(calories)	
+calories.sort()
+print("Calories list has been sorted!")
+print(calories)
+
+		
+	
